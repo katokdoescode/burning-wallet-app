9198c515a5aefce48670192e30d3f65bb97baf1b
katokdoescode <snakedoc.md@gmail.com>
2023-02-02 18:34:38 +0400

    ☕️ Refactor WalletsApi method to make a request from server

--- src/api/WalletsApi.ts
+++ src/api/WalletsApi.ts
@@ -2,7 +2,7 @@ import { pb } from '$pocketbaseConfig';
 import { pickBy } from '$utils/pick';
 
 import type { Pagination } from '$models/PaginationModel';
-import type { Wallets, WalletsWithPagination } from '$models/WalletsModel';
+import type { WalletRecord, Wallets, WalletsWithPagination } from '$models/WalletsModel';
 import type { PaginationRequest } from '$models/PaginationModel';
 
 export class WalletsApi {
@@ -14,9 +14,13 @@ export class WalletsApi {
 
 	async getWallets(searchParams: URLSearchParams) {
 		const pagination = this.searchParamsToObject(searchParams);
+
+		// TODO: Create expand interface
 		const response: Wallets = await pb
 			.collection(this.endpoint)
-			.getList(pagination.page, pagination.perPage);
+			.getList(pagination.page, pagination.perPage, {
+				expand: 'currency'
+			});
 		const resPagination: Pagination = pickBy<Pagination>(response, [
 			'page',
 			'perPage',
@@ -24,8 +28,13 @@ export class WalletsApi {
 			'totalPages'
 		]);
 
+		// TODO: Expand known values
+		const items = response.items.map((item: WalletRecord) =>
+			pickBy<WalletRecord>(item, ['name', 'currency'])
+		);
+
 		const res: WalletsWithPagination = {
-			items: response.items,
+			items,
 			pagination: resPagination
 		};
 
