caae076a0829823de846c4384d92269eba98f169
katokdoescode <snakedoc.md@gmail.com>
2023-02-02 14:56:45 +0400

    🍋 Create pick function to picking any data from server response object

---
+++ src/utils/pick.ts
@@ -0,0 +1,9 @@
+// Same as lodash/pickBy
+export function pickBy<T>(object: object & T, keys: (keyof T)[] ): T {
+	const newObject: T = Object.fromEntries([keys]);
+
+	keys.forEach((key: keyof T) => {
+		newObject[key] = object[key]
+	});
+	return newObject;
+};
